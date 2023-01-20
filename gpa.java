bc3ccd8c036a8d7a416dff46a8638741a1b1ed12
Bloopy! <82061021+Blooper5500@users.noreply.github.com>
2023-01-20 11:14:52 -0700

    Lists Added
    Added a working version of the program that uses a list like the instructions say.

--- gpa.java
+++ gpa.java
@@ -4,24 +4,20 @@
 */
 
 import java.util.Scanner;
-import java.util.ArrayList;
-
 public class gpa
 {
     public static void main(String[] args)
     {
-
-
         Scanner in = new Scanner(System.in);
-        ArrayList<Integer> grades = new ArrayList<Integer>();
         //I know this wasn't how I was shown to create a list in class, but I forgot and when I looked it up this is how I was shown to create one by the internet.
 
 
         System.out.println("How many classes did you have this semester? ");
         int ClassNum = in.nextInt();
+        int[] grades;
+        grades = new int[ClassNum];
 
 
-        int TotalGrade = 0;
         for (int i = 1; i <= ClassNum; i++)
         {
             System.out.println("Is class " + i + " an AP class? (y/n): ");
@@ -31,42 +27,39 @@ public class gpa
                 System.out.println("Enter the letter grade for class " + i + ": ");
                 char Grade = in.next().charAt(0);
                 if (Grade == 'A')
-                    grades.add(5);
+                    grades[i - 1] = 5;
                 else if (Grade == 'B')
-                    grades.add(4);
+                    grades[i - 1] = 4;
                 else if (Grade == 'C')
-                    grades.add(3);
+                    grades[i - 1] = 3;
                 else if (Grade == 'D')
-                    grades.add(2);
+                    grades[i - 1] = 2;
                 else
-                    grades.add(0);
+                    grades[i - 1] = 0;
             }
             else if (APvalue == 'n')
             {
                 System.out.println("Enter the letter grade for class " + i + ": ");
                 char Grade = in.next().charAt(0);
                 if (Grade == 'A')
-                    grades.add(4);
+                    grades[i - 1] = 4;
                 else if (Grade == 'B')
-                    grades.add(3);
+                    grades[i - 1] = 3;
                 else if (Grade == 'C')
-                    grades.add(2);
+                    grades[i - 1] = 2;
                 else if (Grade == 'D')
-                    grades.add(1);
+                    grades[i - 1] = 1;
                 else
-                    grades.add(0);
+                    grades[i - 1] = 0;
             }
         }
-        /* Note: All of this code is useless at the moment.
-        I have a list but no idea how to find the sum or average of it.
-        It's 11pm, I'm tired and want to go to bed :(
-
-
+        int TotalGrade = 0;
+        for (int i = 0; i < ClassNum; i++)
+        {
+            TotalGrade += grades[i];
+        }
         System.out.println("Total Grade: " + TotalGrade);
-        System.out.println("GPA: " + (TotalGrade/ClassNum));
         int FinalGPA = TotalGrade / ClassNum;
         System.out.println(FinalGPA);
-        */
-        System.out.println(grades);
     }
 }
